23adcf032ff0122af07510303eeea552c67d3818
n1ckDotEXE <admin@nicholasnam.com>
2021-03-10 10:47:49 -0500

    Complete

---
+++ src/common/Button.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Button = (props) => {
+	//console.log(props.propsName);
+	return (
+		<button
+			disabled={props.propsButtonToggle}
+			onClick={props.propsOnClick}
+			className={props.propsClassName}
+		>
+			{props.propsName}
+		</button>
+	);
+};
+
+export default Button;
